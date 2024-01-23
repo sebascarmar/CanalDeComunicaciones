414a1bc8346c7ce348d2f021cce9a0c113f38340
sebacarmar <sebacarmar@gmail.com>
2024-01-23 17:35:46 -0300

    Agrega aletoreidad a la elección de la fase

--- src/python/punto_flotante/classes/phase_off.py
+++ src/python/punto_flotante/classes/phase_off.py
@@ -13,12 +13,26 @@ class phase_off:
         self.num_of_symb = 0
 
 
-    def get_phase_off(self):
-        i        = random.randint(0,len(self.titas))
-        ejO      = cmath.exp(1j*self.titas[i])
+    def get_phase_off(self, RRC_tx_I_symb_out, RRC_tx_Q_symb_out):
+        self.symbI = RRC_tx_I_symb_out
+        self.symbQ = RRC_tx_Q_symb_out
+        
+        # Realiza una selección aleatoria del index i cada 4 símbolos recibidos.
+        if(self.num_of_symb%4 == 0):
+            self.num_of_symb = 0
+            self.i           = random.randint(0,len(self.titas))
+        
+        self.num_of_symb += 1
+        print(self.num_of_symb, self.i, self.titas[self.i])
+        
+        # Creación del factor e^(jwt) o, en ptras palabras, e^(jTita).
+        ejO = cmath.exp(1j*self.titas[self.i])
+        
+        # Símbolo proveniente del filtro en su forma rectangular.
         symb_IjQ = self.symbI + self.symbQ*1j
         
-        symb_IjQ_with_off = symb_IjQ*ejO
+        # Generación del desfasaje del símbolo.
+        symb_IjQ_with_off = symb_IjQ * ejO
         
         return (symb_IjQ_with_off.real, symb_IjQ_with_off.imag)
 
