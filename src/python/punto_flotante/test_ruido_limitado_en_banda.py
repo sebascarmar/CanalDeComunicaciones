97fd7d89419c92f67bfcb37c715e1616644a2423
lsoto <sotoluisfranco@gmail.com>
2024-01-23 21:34:07 -0300

    Se agrega test ruido limitado en banda

--- src/python/punto_flotante/test_ruido_limitado_en_banda.py
+++ src/python/punto_flotante/test_ruido_limitado_en_banda.py
@@ -9,22 +9,18 @@ Fc = 1000 # Frecuencia de corte del filtro
 
 # Generar senoide
 t = np.linspace(0, N/Fs, N)
-x = np.sin(2*np.pi*440*t)
 
-# Añadir ruido filtrado en banda
+# Generar ruido
 noise = np.random.randn(N)
 fb, fa = 100, 2000
-filtro_ruido = signal.firwin(151, [fb,fa], fs=Fs, pass_zero=False)
-noise = np.convolve(filtro_ruido, noise)[:N]
-x_ruido = x + noise
 
 # Diseñar y aplicar filtro FIR pasa-bajos
 filtro_lp = signal.firwin(151, Fc, fs=Fs, pass_zero=True)
-x_filtrada = np.convolve(filtro_lp, x_ruido)[:N]
+x_filtrada = np.convolve(filtro_lp, noise)[:N]
 
 # Graficar
 plt.subplot(2,1,1)
-plt.plot(t, x_ruido)
+plt.plot(t, noise)
 plt.title('Señal con ruido')
 
 plt.subplot(2,1,2)
