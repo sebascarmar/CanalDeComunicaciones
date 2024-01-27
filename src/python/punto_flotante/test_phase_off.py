1e0a2aec78c289ee20dbade1d6471ffc1b22b1ad
sebacarmar <sebacarmar@gmail.com>
2024-01-27 13:02:00 -0300

    Bloque de código para test automático

--- src/python/punto_flotante/test_phase_off.py
+++ src/python/punto_flotante/test_phase_off.py
@@ -40,21 +40,29 @@ offset_gen =  phase_off(fs, Delta_f)
 
 
 
+###########################################################################################
+#                                     Test automático                                     #
+###########################################################################################
+Nsymb   = 1000
 
-for j in range(4):
-    for x in range(4):
-        (auxI, auxQ) = offset_gen.get_phase_off(symbI[x], symbQ[x])
-        symbI_des.append(auxI)
-        symbQ_des.append(auxQ)
-    
-    plt.figure(figsize=[6,6])
-    plt.plot(symbI, symbQ, 'bo', linewidth=2.0, label=b'Constelacion original')
-    plt.plot(symbI_des, symbQ_des, 'r.', linewidth=2.0, label=r'Constelacion original')
-    plt.xlim((-2, 2))
-    plt.ylim((-2, 2))
-    plt.grid(True)
-    plt.xlabel('Real')
-    plt.ylabel('Imag')
-    
-    plt.show()
+symbolsI = 2*(np.random.uniform(-1,1,Nsymb)>0.0)-1;
+symbolsQ = 2*(np.random.uniform(-1,1,Nsymb)>0.0)-1;
 
+symbI_des = []
+symbQ_des = []
+for x in range(len(symbolsI)):
+    (auxI, auxQ) = offset_gen.get_phase_off(symbolsI[x], symbolsQ[x])
+    symbI_des.append(auxI)
+    symbQ_des.append(auxQ)
+
+offset = 0
+plt.figure(figsize=[6,6])
+plt.plot(symbolsI, symbolsQ, 'bo', linewidth=2.0, label=b'Constelacion original')
+plt.plot(symbI_des, symbQ_des, 'r.', linewidth=2.0, label=b'Constelacion desfasada')
+plt.xlim((-2, 2))
+plt.ylim((-2, 2))
+plt.grid(True)
+plt.xlabel('Real')
+plt.ylabel('Imag')
+
+plt.show()
