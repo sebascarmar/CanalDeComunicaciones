a3be1c8586b8d0624b0c92ab3c496c5cc35c34eb
lsoto <sotoluisfranco@gmail.com>
2024-03-01 19:44:30 -0300

    Python y documentos auxiliares para el testeo del modulo gng

--- src/RTL_Modulos/gng_master/histogram.py
+++ src/RTL_Modulos/gng_master/histogram.py
@@ -2,37 +2,31 @@ import matplotlib.pyplot as plt
 
 data = []
 
-shifter_divider = -1
+sigma = ''
 
 with open('D:/run/gng_test/datos.txt') as f:
   for line in f: 
 
     num = int(line)
 
-    if (shifter_divider == -1):
-       shifter_divider = num
+    if (sigma == ''):
+       sigma = num
        continue
 
     #print(f"num = {num}, line = {line}")
 
     # Separar parte entera, fraccional y signo
-    sign_mask = 0x8000>>shifter_divider
-    signo = (num & sign_mask) >> (15-shifter_divider)
+    signo = (num & 0x80000000) >> 31
 
-    if (shifter_divider < 4): # con parte entera
-        integer_mask = (0xF>>shifter_divider) << 11
-        integer = (num & integer_mask) >> 11
-        fractional = (num & 0x07FF) / (2**11)
-    else:   # sin parte entera, solo parte fraccional
-        fractional_mask = (0x7FF>>(shifter_divider-4)) 
-        integer = 0
-        fractional = (num & fractional_mask) / (2**11)
+    integer = (num & 0x7FC00000) >> 22
+    fractional = (num & 0x003FFFFF) / (2**22)
     
-    #print(f"signo = {signo}, integer = {integer}, fractional = {fractional}")
+    #print(f"num = {num}, signo = {signo}, integer = {integer}, fractional = {fractional}")
 
     # Convertir a float
     if (signo == 1):
-        num_float = (-16/(2**shifter_divider)) + integer + fractional
+        pass
+        num_float = -(2**9) + integer + fractional
     else:
         num_float = integer + fractional
 
